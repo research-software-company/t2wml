0702743cdb2c0063bfd660859fbc7a0b18a31855
Itay Zandbank <itay@chelem.co.il>
2020-10-15 18:35:53 +0300

    Trying to get the app to start

--- electron/tests/async.test.ts
+++ electron/tests/async.test.ts
@@ -6,8 +6,34 @@ import chaiAsPromised from 'chai-as-promised';
 chai.should();
 chai.use(chaiAsPromised);
 
+import { initializeSpectronApp } from './utils';
+const app = initializeSpectronApp();
+
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-describe('')
+before('Try Before', async () => {
+    console.log('before block here');
+    chaiAsPromised.transferPromiseness = (app as any).transferPromiseness;  // transferPromiseness is not defined in the type
+    await app.start();
+    console.log('before block done');
+});
+
+describe('Actual Test Group', () => {
+    it('Sync test', () => {
+        const x = 5;
+        expect(x).to.equal(5);
+    });
+    it('Async test', async () => {
+        console.log('Async test starting');
+        const x = 5;
+        await delay(5000);
+        expect(x).to.be.equal(5);
+        console.log('Async test done');
+    });
+    it('Second sync test', () => {
+        const x = 5;
+        expect(x).to.equal(5);
+    });
+});
